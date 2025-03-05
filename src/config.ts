aca56c859e9b7913532cb1fbbec9fd313902036a
AstridNielsen-lab <juliocamposmachado@gmail.com>
2025-03-05 18:41:47 -0300

    Resolvendo conflitos antes do push

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent";
-export const API_KEY = "AIzaSyAuFi5KtPsMJI5IC8c5FjvYD5IbuBdwH_U";
+export const API_KEY = "AIzaSyCXZu1FqQ04yzMzRCRk78nzUtEtGkWp7zc";
 
 export const SUPPORTED_LANGUAGES = [
   'javascript',
